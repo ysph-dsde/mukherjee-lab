9aabba6705b270622b7b9a3cf645614c3900acf9
howardbaik <howardhowiebaek@gmail.com>
2024-09-22 23:34:44 -0400

    Try

--- _utils.R
+++ _utils.R
@@ -4,13 +4,6 @@ library(dplyr)
 library(readr)
 library(fontawesome)
 
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  warning = FALSE,
-  message = FALSE,
-  fig.retina = 3,
-  comment = "#>"
-)
 
 make_doi_link <- function(citation) {
   
